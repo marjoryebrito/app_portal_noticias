b0cb5455d0a7449696a03ff0b64b63a3aabdadf3
marjoryebrito <marjoryebrito@protonmail.com>
2022-03-03 20:59:28 -0300

    Armazenando consultas em cache parte 2.

--- app/Http/Controllers/NoticiaController.php
+++ app/Http/Controllers/NoticiaController.php
@@ -16,13 +16,22 @@ class NoticiaController extends Controller
      */
     public function index()
     {
+       
+        /*Cache::put('site', 'teste.com.br', 10);
+       
+        echo Cache::get('site');*/
+
         $noticias = [];
+       
 
-        Cache::put('site', 'teste.com.br', 10);
-       // $noticias = Noticia::orderByDesc('created_at')->limit(10)->get();
+        if(Cache::has('dez_primeiras_noticias')){
+            $noticias = Cache::get('dez_primeiras_noticias');
+        }else{
+            $noticias = Noticia::orderByDesc('created_at')->limit(10)->get();
+            Cache::put('dez_primeiras_noticias', $noticias, 15);
+        }
 
-        echo Cache::get('site');
-
+        
         return view('noticia', ['noticias' => $noticias]);
     }
 
