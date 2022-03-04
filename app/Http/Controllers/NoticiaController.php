44f5bcf6da74962e574d24a57c5e92a92b1deaa1
marjoryebrito <marjoryebrito@protonmail.com>
2022-03-03 21:05:22 -0300

    Cache remember.

--- app/Http/Controllers/NoticiaController.php
+++ app/Http/Controllers/NoticiaController.php
@@ -16,22 +16,22 @@ class NoticiaController extends Controller
      */
     public function index()
     {
-       
-        /*Cache::put('site', 'teste.com.br', 10);
-       
-        echo Cache::get('site');*/
-
+      
         $noticias = [];
        
 
-        if(Cache::has('dez_primeiras_noticias')){
+       /* if(Cache::has('dez_primeiras_noticias')){
             $noticias = Cache::get('dez_primeiras_noticias');
         }else{
             $noticias = Noticia::orderByDesc('created_at')->limit(10)->get();
             Cache::put('dez_primeiras_noticias', $noticias, 15);
-        }
+        }*/
 
         
+        $noticias = Cache::remember('dez_primeiras_noticias', 15, function(){
+            return Noticia::orderByDesc('created_at')->limit(10)->get();
+        });
+
         return view('noticia', ['noticias' => $noticias]);
     }
 
